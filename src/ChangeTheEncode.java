a987bc4133075782e0f81af9fb012034dfd303d1
Gahon <1053475617@qq.com>
2017-03-25 00:14:07 +0800

    Change the floder

---
+++ src/ChangeTheEncode.java
@@ -0,0 +1,34 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.Collection;
+
+import org.apache.commons.io.FileUtils;
+
+public class ChangeTheEncode {
+
+	public static void main(String[] args) {
+		// 用commons-io.jar实现文件的
+		// GBK编码格式源码路径
+		String srcDirPath = "/Users/gahon/git/FiveChess/src/";
+		// 转为UTF-8编码格式源码路径
+		String utf8DirPath = "/Users/gahon/git/FiveChess/src/UTF8/";
+
+		// 获取所有java文件
+		Collection<File> javaGbkFileCol =  FileUtils.listFiles(new File(srcDirPath), new String[]{"java"}, true);
+
+		for(File javaGbkFile:javaGbkFileCol)
+		{
+			// UTF8格式文件路径
+			String utf8FilePath = utf8DirPath + javaGbkFile.getAbsolutePath().substring(srcDirPath.length());
+			
+			// 使用GBK读取数据，然后用UTF-8写入数据
+			try {
+				FileUtils.writeLines(new File(utf8FilePath), "UTF-8", FileUtils.readLines(javaGbkFile, "GBK"));
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		System.out.println("转换完成");
+	}
+}
